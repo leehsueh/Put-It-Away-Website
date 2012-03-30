5373d2a144779b6a5d5c38a44a4e941da81799bf
Hain-Lee Hsueh <leehsueh7@gmail.com>
2012-03-30 02:26:35 -0700

    added modal dialog informing of pending update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,22 @@
 
 */
 
-
+$(document).ready(function() {
+	var bgOverlay = $('#lbOverlay');
+	var updateNote = $('#update-note');
+	bgOverlay.css('opacity','0');
+	bgOverlay.show().animate({
+		opacity:0.6,
+	}, 500, function() {
+		updateNote.fadeIn(200);
+	});
+	var closeLink = updateNote.find('a');
+	closeLink.click(function(event) {
+		event.preventDefault();
+		updateNote.fadeOut(200);
+		bgOverlay.fadeOut(200);
+	});
+});
 
 
 
